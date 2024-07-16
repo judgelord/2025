01a05df05f7af07aae610f19198d8e8bf3394bd8
judgelord <devin.jl@gmail.com>
2024-07-16 16:36:38 -0400

    improve parsing

---
+++ code/update.R
@@ -0,0 +1,11 @@
+# source all scripts needed to reconstruct data
+
+source("code/read_2025_txt.R")
+
+source("code/get_section_agency_crosswalk.R")
+
+source("code/get_keywords.R")
+
+source("code/parse.R")
+
+source("code/count_racialized_terms.R")
